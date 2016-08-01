99db8b71744797373b937015eb17707fbc344a16
wuy <529032234@qq.com>
2016-08-01 14:16:19 +0800

    badword

---
+++ BadWord/BadWord/BadWord/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FoundationHelper;
+
+namespace BadWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "bad.txt";
+            FoundationHelper.FilterWord filter = new FoundationHelper.FilterWord(path);
+            filter.SourctText = textBox1.Text;
+            filter.Filter('*');
+            int count= filter.IllegalWords.Count;
+        }
+    }
+}
